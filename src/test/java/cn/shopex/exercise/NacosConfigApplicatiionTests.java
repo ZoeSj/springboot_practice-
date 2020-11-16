22f31867defd4a509cb2533053fdce2392b2aca3
盛佳 <1368329355@qq.com>
2020-11-16 11:53:12 +0800

    [init test]

--- src/test/java/cn/shopex/exercise/NacosConfigApplicatiionTests.java
+++ src/test/java/cn/shopex/exercise/NacosConfigApplicatiionTests.java
@@ -1,10 +1,19 @@
 package cn.shopex.exercise;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 /**
  * @Auther shengjia
  * @Create 2020/11/16
  * @description:
  **/
 
-public class NacosConfigApplicatiionTests {
+@SpringBootTest
+class NacosConfigApplicatiionTests {
+
+    @Test
+    void contextLoads() {
+    }
+
 }
